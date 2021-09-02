0f5b2cda75e7b4d521cf3fd624282d989715a5ac
Caonibaba233 <595765852@qq.com>
2021-09-02 15:11:56 +0800

    2021-09-02

--- src/September_1/demo_1.java
+++ src/September_1/demo_1.java
@@ -2,6 +2,8 @@ package September_1;
 
 import org.junit.Test;
 
+import java.util.Scanner;
+
 /**
  * 斐波那契数，通常用 F(n) 表示，形成的序列称为 斐波那契数列 。
  * 该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
@@ -47,6 +49,14 @@ public class demo_1 {
             r = p + q;
         }
         return r;
+    }
+
+
+    @Test
+    public void run1(){
+        char[] str = {'1','2','3'};
+        int res = new Integer(new String(str)).intValue();
+        System.out.println("结果：" + res);
     }
 
 }
